cb9a13cd0350c24892abca519fa469fc6bf31038
Wonseok Park <amurorei2000@gmail.com>
2022-12-19 16:18:57 +0900

    Initial commit

---
+++ Source/Shooting/Private/CodeTest.cpp
@@ -0,0 +1,167 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CodeTest.h"
+
+#pragma warning (disable: 4458)	// c4458 경고 끄기
+
+// 기본 값을 설정할 수 있다.
+// 생성자 함수
+ACodeTest::ACodeTest()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	age = 42;
+	height = 180;
+}
+
+// Called when the game starts or when spawned
+void ACodeTest::BeginPlay()
+{
+	Super::BeginPlay();
+
+#pragma region 사칙연산 
+
+	//// Add 함수에 입력값 8, 4를 넣어서 결과를 result 변수에 받는다.
+	//Add(8, 4);
+	//int32 result2 = Subtract(8, 4);
+	//int32 result3 = Multiply(8, 4);
+	//float result4 = Divide(4, 8);
+
+	//// result 변수에 있는 값을 화면에 출력한다.
+	////UE_LOG(LogTemp, Warning, TEXT("Result1 : %d"), result1);
+	//UE_LOG(LogTemp, Warning, TEXT("Result2 : %d"), result2);
+	//UE_LOG(LogTemp, Warning, TEXT("Result3 : %d"), result3);
+	//UE_LOG(LogTemp, Warning, TEXT("Result4 : %.1f"), result4);
+
+
+	//// 만일, 나이가 30살 이상이면 "할배"라고 출력하고 싶다.
+	//if (age >= 30)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("할배다~"));
+	//}
+	//// 그렇지 않고 만일, 나이가 20살 이상이면 "꼰대"라고 출력하고 싶다.
+	//else if (age >= 20)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("꼰대시군요~"));
+	//}
+	//// 그렇지 않다면, "꼬맹이"라고 출력하고 싶다.
+	//else
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("잼민이네~"));
+	//}
+#pragma endregion
+
+#pragma region 조건식
+
+
+
+	//// 만일 나이가 20살 이상이면서, 키가 160 이상인 경우에는
+	//if (age >= 20 || height >= 160)
+	//{
+	//	// "환영"이라고 출력한다.
+	//	UE_LOG(LogTemp, Warning, TEXT("환영합니다~~~!!"));
+	//}
+	//// 그렇지 않으면,
+	//else
+	//{
+	//	// "외면"이라고 출력한다.
+	//	UE_LOG(LogTemp, Warning, TEXT("실례했습니다."));
+	//}
+
+	// 국적이 "한국"이면서, 나이가 20살 이상 40세 미만일 때,
+	//if (nation == TEXT("한국"))
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("한국인"));
+	//	
+	//	if (age >= 20 && age < 40)
+	//	{
+	//		// "청년"이라고 출력한다.
+	//		UE_LOG(LogTemp, Warning, TEXT("청년!"));
+	//	}
+	//}
+	//// 그렇지 않으면,
+	//else
+	//{
+	//	// "청년이 아니다"라고 출력한다.
+	//	UE_LOG(LogTemp, Warning, TEXT("청년이 아니시군요..."));
+	//}
+
+	/*FString result = StringAppender(TEXT("박원석"), TEXT("멋쟁이"));
+	UE_LOG(LogTemp, Warning, TEXT("함수 실행 결과: %s"), *result);*/
+
+	//int32 checker = 123;
+
+	//// 만일, checker 변수의 값이 짝수라면
+	//if (checker % 2 == 0)
+	//{
+	//	// "even"이라고 출력한다.
+	//	UE_LOG(LogTemp, Warning, TEXT("even"));
+	//}
+	//// 그렇지 않고, 변수의 값이 홀수라면
+	//else
+	//{
+	//	// "odd"라고 출력한다.
+	//	UE_LOG(LogTemp, Warning, TEXT("odd"));
+	//}
+
+#pragma endregion
+
+	UE_LOG(LogTemp, Warning, TEXT("%d"), Add(3, 1));
+
+	// 반복문
+	// "안녕하세요"라는 문자열을 10번 출력한다.
+	//MulitplicationTable(7);
+
+	for (int32 i = 2; i < 10; i++)
+	{
+		for (int32 j = 1; j < 10; j++)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%d x %d = %d"), i, j, i * j);
+		}
+		UE_LOG(LogTemp, Warning, TEXT("==================================="));
+	}
+}
+
+void ACodeTest::MulitplicationTable(int32 guguNum)
+{
+	for (int32 i = 1; i < 10; i++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%d x %d = %d"), guguNum, i, guguNum * i);
+	}
+}
+
+// Called every frame
+void ACodeTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+int32 ACodeTest::Add(int32 num1, int32 num2)
+{
+	// to do
+	int32 result = num1 + num2;
+	return result;
+}
+
+int32 ACodeTest::Subtract(int32 num1, int32 num2)
+{
+	return num1 - num2;
+}
+
+int32 ACodeTest::Multiply(int32 num1, int32 num2)
+{
+	return num1 * num2;
+}
+
+float ACodeTest::Divide(int32 num1, int32 num2)
+{
+	return (float)num1 / (float)num2;
+}
+
+FString ACodeTest::StringAppender(FString a, FString b)
+{
+	return a + b;
+}
---
+++ Source/Shooting/Public/CodeTest.h
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CodeTest.generated.h"
+
+UCLASS()
+class SHOOTING_API ACodeTest : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACodeTest();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// 정수형 변수 number를 선언한다.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CodeVarible)
+	int32 number;
+	//int32 number = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=CodeVarible)
+	int32 number2;
+
+	// 실수형 변수 fnumber를 선언한다.
+	float fnumber1;
+	//float fnumber1 = 0;
+	float fnumber2 = 10;
+	
+	// 문자열 변수 name을 선언한다.
+	FString name;
+	//FString name = "";
+	FString name2 = FString(TEXT("박원석"));
+	//FString name2 = FString("박원석");
+
+	// 논리형 변수 isStudent를 선언한다.
+	bool isStudent;
+	//bool isStudent = false;
+	bool isTeacher = true;
+
+	int32 Add(int32 num1, int32 num2);
+	int32 Subtract(int32 num1, int32 num2);
+	int32 Multiply(int32 num1, int32 num2);
+	float Divide(int32 num1, int32 num2);
+
+	int32 result = 100;
+
+	UPROPERTY(EditAnywhere, Category= CodeVarible)
+	int32 age;
+
+	UPROPERTY(EditAnywhere, Category = CodeVarible)
+	int32 height;
+
+	UPROPERTY(EditAnywhere, Category = CodeVarible)
+	FString nation = FString(TEXT("한국"));
+
+	// 문자열 두 개를 받아서 문자열 한 개로 합쳐서 반환하는 함수를 만들고 싶다.
+	UFUNCTION(BlueprintCallable)
+	FString StringAppender(FString a, FString b);
+
+	void MulitplicationTable(int32 guguNum);
+
+private:
+
+
+};
